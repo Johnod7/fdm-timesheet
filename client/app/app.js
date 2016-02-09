e9f79b6804ffbbb32a5816587c1e6c8c171b6bfc
Johnod7 <john.overdose@gmail.com>
2016-02-09 09:55:18 +0000

    newest framework push

---
+++ client/app/app.js
@@ -0,0 +1,11 @@
+angular.module('fdmTimesheet', [
+    'ngAria',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ui.router',
+    'ui.bootstrap',
+    'duScroll',
+    'ngMessages'
+  ])
+  
